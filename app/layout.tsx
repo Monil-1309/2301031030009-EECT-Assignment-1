1738a23ba5d9f6f5fe52cded578404cd55646f78
Monil-1309 <monildesai580@gmail.com>
2025-09-04 17:22:10 +0530

    feat: add gtm script

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PJ2Q8RPN');
+            `,
+          }}
+        />
         <link rel="icon" href="/elixlifestyle.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PJ2Q8RPN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
